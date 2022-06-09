c8bcaa094fdb7df797c4f2369b6d066bd8519fa1
Angelina <lunina.angelinka2002@gmail.com>
2022-06-09 18:06:55 +0200

    09.06

---
+++ ShooterGame/Assets/Resourses/PlayerInfo.cs
@@ -0,0 +1,16 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//[CreateAssetMenu (fileName = "ItemDB", menuName ="dataBase", order = 2 )]
+//public class PlayerInfo : ScriptableObject
+//{
+//    public List<ItemsBase> ItemsBD = new List<ItemsBase>();
+//}
+
+//[ System.Serializable]
+
+//public class ItemsBase{
+//    public string namePlayer;
+//    public int id;
+//   // public int score;
+//}
